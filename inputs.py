4004c026f1417c8202f3697d27e36080492aa958
Heiðar Sigurjónsson <heidars19@ru.is>
2019-09-17 18:38:49 +0000

    can_move func ready

---
+++ inputs.py
@@ -0,0 +1,15 @@
+def movement(pos_north, pos_south, pos_west, pos_east) :
+    possible_directions = ""
+    if pos_north :
+        possible_directions += " or (N)orth" 
+    if pos_west :
+        possible_directions += " or (W)est" 
+    if pos_east :
+        possible_directions += " or (E)ast" 
+    if pos_south :
+        possible_directions += " or (S)outh"
+    possible_directions = "You can travel: " + possible_directions.replace(" or ", "", 1) + "."
+    return possible_directions
+
+print(movement(True, False, False, True))
+    

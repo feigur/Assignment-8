cde8de7c8209b762dd11cb523ee636704e610a5d
Heiðar Sigurjónsson <heidars19@ru.is>
2019-09-17 18:53:33 +0000

    can_move fansyfied

--- inputs.py
+++ inputs.py
@@ -1,4 +1,4 @@
-def movement(pos_north, pos_south, pos_west, pos_east) :
+def can_move(pos_north, pos_south, pos_west, pos_east) :
     possible_directions = ""
     if pos_north :
         possible_directions += " or (N)orth" 
@@ -11,5 +11,4 @@ def movement(pos_north, pos_south, pos_west, pos_east) :
     possible_directions = "You can travel: " + possible_directions.replace(" or ", "", 1) + "."
     return possible_directions
 
-print(movement(True, False, False, True))
     

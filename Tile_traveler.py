630fe162def15e33bbcbb9c540c34bca339b4b74
Ófeigur <ofeigur95@gmail.com>
2019-09-18 10:55:49 +0000

    final

--- Tile_traveler.py
+++ Tile_traveler.py
@@ -34,12 +34,12 @@ def can_move(pos_north, pos_south, pos_west, pos_east) :
     possible_directions = ""
     if pos_north :
         possible_directions += " or (N)orth" 
-    if pos_west :
-        possible_directions += " or (W)est" 
     if pos_east :
         possible_directions += " or (E)ast" 
     if pos_south :
         possible_directions += " or (S)outh"
+    if pos_west :
+        possible_directions += " or (W)est" 
     possible_directions = "You can travel: " + possible_directions.replace(" or ", "", 1) + "."
     print(possible_directions)
 
